faea685b544e7be61b495f14ff5f831152b27654
henry-dura <henryikemefuna@gmail.com>
2023-02-07 13:52:16 +0100

    Add meals dynamically

--- src/modules/GetMeal.js
+++ src/modules/GetMeal.js
@@ -1,10 +1,27 @@
-const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken';
+const cardContainer = document.querySelector(".cards");
 
-const getData = async() =>{
-    const response = await fetch(url);
-    const data = await response.json();
-    return data.meals;
-}
+const url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken";
 
+const getData = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.meals;
+};
 
+const displayInHtml = async () => {
+  const meals = await getData();
+  meals.forEach((meal) => {
+    console.log(meal.strMealThumb)
+    const div = document.createElement("div");
+    div.classList.add("item");
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" alt="Meal-picture">
+                        <div class="name"><p>${meal.strMeal}</p><i class="fa-regular fa-heart"></i></div>
+                        <p class="likes">7 Likes</p>
+                        <button class="comment">Comments</button>
+    `;
+    cardContainer.appendChild(div);
+  });
+};
 
+export default displayInHtml;
