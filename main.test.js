974c75c7f1a284e8cd3c93c47b5c691c9a439347
James Serengia <jamesserengia@gmail.com>
2023-02-09 19:26:11 +0300

    Updated main testing file

--- main.test.js
+++ main.test.js
@@ -1,13 +1,13 @@
 const { expect, describe } = require("@jest/globals");
 const getComments = require("./__mocks__/getComments.js");
 
-const testArr = [1, 2, 3, 4];
+const testArr = [{}, {}, {}, {}];
 
 describe("Testing comments count", () => {
   test("should return first value of arr", async () => {
     const result = await getComments(testArr);
 
-    expect(result[0]).toBe(1);
+    expect(result[0]).toEqual({});
   });
 
   test("should return arr length", async () => {
